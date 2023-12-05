59153d53f7ac855e0c0a187d33c35594b1a1cce0
a23sergiopg <serprigar@gmail.com>
2023-12-05 14:23:04 +0100

    feat(randomize_list.py): create the program to randomize the xspf list

---
+++ VLC_PlayShuffle/randomize_list.py
@@ -0,0 +1,6 @@
+from random import sample
+def randomize_xspf(xspf_list):
+    assert isinstance(xspf_list, list)
+    randomized_list = sample(xspf_list, k=(len(xspf_list)))
+    assert isinstance(randomized_list, list)
+    return randomized_list
