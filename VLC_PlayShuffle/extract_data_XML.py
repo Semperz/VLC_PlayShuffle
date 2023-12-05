ef6bde71a7ec70836f20f5c18b04df3de42424d6
a23sergiopg <serprigar@gmail.com>
2023-12-05 14:21:40 +0100

    feat(extract_data_XML.py): create the program to extract the main data from xspf file

--- VLC_PlayShuffle/extract_data_XML.py
+++ VLC_PlayShuffle/extract_data_XML.py
@@ -1,5 +1,11 @@
 import xml.etree.ElementTree as ET
-
-def parse_xspf(parsed):
+def extract_xspf_list(parsed_xspf):
     tree = ET.parse('playlistVLC.xspf')
-    root = tree.getroot()
+    root_xspf = tree.getroot()
+    xspf_list =[]
+    for track in root_xspf.iter('{http://xspf.org/ns/0/}track'):
+        location = track.find('{http://xspf.org/ns/0/}location').text
+        xspf_list.append(location)
+    return xspf_list
+
+
