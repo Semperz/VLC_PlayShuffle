8ee069ddb8b178c305dd74facbac8f830424e52e
Sergio Prieto <serprigar@gmail.com>
2023-12-06 20:44:56 +0100

    refactor(extract_data_XML.py): change variable names

--- VLC_PlayShuffle/extract_data_XML.py
+++ VLC_PlayShuffle/extract_data_XML.py
@@ -1,11 +1,10 @@
 import xml.etree.ElementTree as ET
-def extract_xspf_list(parsed_xspf):
-    tree = ET.parse('playlistVLC.xspf')
-    root_xspf = tree.getroot()
+def extract_xspf_list():
+    parsed_xspf = ET.parse('playlistVLC.xspf')
+    root_xspf = parsed_xspf.getroot()
     xspf_list =[]
     for track in root_xspf.iter('{http://xspf.org/ns/0/}track'):
         location = track.find('{http://xspf.org/ns/0/}location').text
         xspf_list.append(location)
     return xspf_list
-
 
