b6639b45b4e10a6b84be2566595940149bef7f1d
Sergio Prieto <serprigar@gmail.com>
2023-12-07 19:40:36 +0100

    refactor(randomize_list.py): change the location of the file

--- src/randomize_list.py
+++ src/randomize_list.py
@@ -1,5 +1,5 @@
 from random import sample
-from extract_data_XML import extract_xspf_list
+from extract_data_xml import extract_xspf_list
 def randomize_xspf():
     assert isinstance(extract_xspf_list(), list)
     randomized_list = sample(extract_xspf_list(), k=(len(extract_xspf_list())))
