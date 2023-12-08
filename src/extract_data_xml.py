a520d6267682b5aa2971047bc3ad4c22e414c917
Sergio Prieto <serprigar@gmail.com>
2023-12-08 21:26:18 +0100

    refactor(extract_data_xml.py): modify structure

--- src/extract_data_xml.py
+++ src/extract_data_xml.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
-def extract_xspf_list():
-    parsed_xspf = ET.parse('playlistVLC.xspf')
+def parse_xspf(file):
+    parsed_xspf = ET.parse(file)
     root_xspf = parsed_xspf.getroot()
     xspf_list = []
     for track in root_xspf.iter('{http://xspf.org/ns/0/}track'):
