48816a74c23ed02738ca458f4a1ae0d086257d12
Sergio Prieto <serprigar@gmail.com>
2023-12-09 01:47:50 +0100

    fix(song_randomizer.py): fix file accessibility problem

--- song_randomizer.py
+++ song_randomizer.py
@@ -1,11 +1,17 @@
-from src.randomize_list import randomize_xspf
-from src.extract_data_xml import parse_xspf
-from src.path_maker import path_maker
-import subprocess
+if __name__ == '__main__':
+    from src.randomize_list import randomize_xspf
+    from src.extract_data_xml import parse_xspf
+    from src.path_maker import path_maker
+
+if __name__ != '__main__':
+    from .src.randomize_list import randomize_xspf
+    from .src.extract_data_xml import parse_xspf
+    from .src.path_maker import path_maker
+
 def song_randomizer(root):
+    import subprocess
+    assert root.endswith('.xspf') == True
     songs_path = parse_xspf(root)
     random_order_songs = randomize_xspf(songs_path)
     vlc_execution = path_maker(random_order_songs)
     return subprocess.run(vlc_execution)
-
-print(song_randomizer('playlist.xspf'))
