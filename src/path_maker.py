4c52b25561e4e0496b62b3afd6ef77bee837d9a7
Sergio Prieto <serprigar@gmail.com>
2023-12-09 01:42:36 +0100

    perf(path_maker.py): modify to add macOS and Linux VLC paths

--- src/path_maker.py
+++ src/path_maker.py
@@ -1,4 +1,13 @@
+import platform
 def path_maker(song_paths):
-    command_win = ['"C:\Program Files\VideoLAN\VLC\VLC.exe"']
-    command_win.extend(song_paths)
-    return command_win
+    if platform.system() == 'Windows':
+        vlc_path = [r'C:\Program Files\VideoLAN\VLC\VLC.exe']  #Windows
+    elif platform.system() == 'Darwin':
+        vlc_path = [r'/Applications/VLC.app/Contents/MacOS/VLC']  #MacOS
+    elif platform.system() == 'Linux':
+        vlc_path = [r'/usr/bin/vlc']  #Linux
+    else:
+        raise NotImplementedError("Program is not supported on the current operating system.")
+
+    vlc_path.extend(song_paths)
+    return vlc_path
