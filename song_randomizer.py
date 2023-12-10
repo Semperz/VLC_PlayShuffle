ce4bd7983f947452faab756d2ae866fbfa12be68
Sergio Prieto <serprigar@gmail.com>
2023-12-10 20:59:07 +0100

    feat(song_randomizer.py): add new asserts and the function call

--- song_randomizer.py
+++ song_randomizer.py
@@ -11,7 +11,11 @@ if __name__ != '__main__':
 def song_randomizer(root):
     import subprocess
     assert root.endswith('.xspf') == True
+    assert isinstance(root,str)
     songs_path = parse_xspf(root)
     random_order_songs = randomize_xspf(songs_path)
     vlc_execution = path_maker(random_order_songs)
+    assert isinstance(vlc_execution, list)
     return subprocess.run(vlc_execution)
+
+song_randomizer('media/playlist.xspf')
