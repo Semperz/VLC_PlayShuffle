3082f34700b612e4a4209af60e2ccffca1e719b7
Sergio Prieto <serprigar@gmail.com>
2023-12-10 20:51:32 +0100

    feat(extract_data_xml.py): add new barricade with 'try' and 'except'

--- src/extract_data_xml.py
+++ src/extract_data_xml.py
@@ -1,10 +1,17 @@
 import xml.etree.ElementTree as ET
 def parse_xspf(file):
-    assert file.endswith('.xspf') == True
-    parsed_xspf = ET.parse(file)
-    root_xspf = parsed_xspf.getroot()
-    xspf_list = []
-    for track in root_xspf.iter('{http://xspf.org/ns/0/}track'):
-        location = track.find('{http://xspf.org/ns/0/}location').text
-        xspf_list.append(location)
-    return xspf_list
+    try:
+        assert file.endswith('.xspf') == True
+        parsed_xspf = ET.parse(file)
+        root_xspf = parsed_xspf.getroot()
+        xspf_list = []
+        for track in root_xspf.iter('{http://xspf.org/ns/0/}track'):
+            location = track.find('{http://xspf.org/ns/0/}location').text
+            xspf_list.append(location)
+        return xspf_list
+    except ET.ParseError as failedParse:
+        print(f'Error parsing xspf file:{failedParse}')
+        return None
+    except AssertionError as notListError:
+        print(f'Error: {notListError}')
+        return None
