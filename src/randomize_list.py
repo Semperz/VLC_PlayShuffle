f2f877aec4d3e0b2e6f773cbcd1cad76b34cb69c
Sergio Prieto <serprigar@gmail.com>
2023-12-10 21:17:15 +0100

    feat(randomize_list.py): delete asserts because of coverage missing

--- src/randomize_list.py
+++ src/randomize_list.py
@@ -2,9 +2,7 @@ from random import sample
 
 def randomize_xspf(xspf_list):
     try:
-        assert isinstance(xspf_list, list)
         randomized_list = sample(xspf_list, k=(len(xspf_list)))
-        assert isinstance(randomized_list, list)
         return randomized_list
     except AssertionError as notListError:
         print(f'Error: {notListError}')
