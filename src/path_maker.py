8973863985f3629c2f8b4de003ea855c0781e4c0
Sergio Prieto <serprigar@gmail.com>
2023-12-10 20:54:33 +0100

    feat(path_maker.py): add new precondition and postcondition to make sure that the list stays as a list in the process

--- src/path_maker.py
+++ src/path_maker.py
@@ -1,5 +1,6 @@
 import platform
 def path_maker(song_paths):
+    assert isinstance(song_paths,list)
     if platform.system() == 'Windows':
         vlc_path = [r'C:\Program Files\VideoLAN\VLC\VLC.exe']  #Windows
     elif platform.system() == 'Darwin':
@@ -10,4 +11,5 @@ def path_maker(song_paths):
         raise NotImplementedError("Program is not supported on the current operating system.")
 
     vlc_path.extend(song_paths)
+    assert isinstance(vlc_path, list)
     return vlc_path
