380559af571c86fdfe467ab7d2e17180066be3d1
Sergio Prieto <serprigar@gmail.com>
2023-12-10 20:56:13 +0100

    feat(randomize_list.py): add barricade with 'try' and 'except'

--- src/randomize_list.py
+++ src/randomize_list.py
@@ -1,8 +1,11 @@
 from random import sample
 
-
 def randomize_xspf(xspf_list):
-    assert isinstance(xspf_list, list)
-    randomized_list = sample(xspf_list, k=(len(xspf_list)))
-    assert isinstance(randomized_list, list)
-    return randomized_list
+    try:
+        assert isinstance(xspf_list, list)
+        randomized_list = sample(xspf_list, k=(len(xspf_list)))
+        assert isinstance(randomized_list, list)
+        return randomized_list
+    except AssertionError as notListError:
+        print(f'Error: {notListError}')
+        return None
