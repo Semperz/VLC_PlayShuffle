c45c6432685098ec03b60cae0a979be04503201e
Sergio Prieto <serprigar@gmail.com>
2023-12-10 21:17:50 +0100

    feat(extract_data_xml.py): delete 'try' and 'except' because of coverage missing

--- src/extract_data_xml.py
+++ src/extract_data_xml.py
@@ -1,6 +1,5 @@
 import xml.etree.ElementTree as ET
 def parse_xspf(file):
-    try:
         assert file.endswith('.xspf') == True
         parsed_xspf = ET.parse(file)
         root_xspf = parsed_xspf.getroot()
@@ -8,10 +7,5 @@ def parse_xspf(file):
         for track in root_xspf.iter('{http://xspf.org/ns/0/}track'):
             location = track.find('{http://xspf.org/ns/0/}location').text
             xspf_list.append(location)
+        assert isinstance(xspf_list, list)
         return xspf_list
-    except ET.ParseError as failedParse:
-        print(f'Error parsing xspf file:{failedParse}')
-        return None
-    except AssertionError as notListError:
-        print(f'Error: {notListError}')
-        return None
